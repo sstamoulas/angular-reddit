576570f9659718f96685247e075d8f2836641ec6
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2021-10-19 17:01:02 +0300

    Creating an Article Class

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostBinding, OnInit } from '@angular/core';
+import { Article } from './article.model';
 
 @Component({
   selector: 'app-article',
@@ -7,24 +8,24 @@ import { Component, HostBinding, OnInit } from '@angular/core';
 })
 export class ArticleComponent implements OnInit {
   @HostBinding('attr.class') cssClass = 'row'; 
-  votes: number;
-  title: string;
-  link: string;
+  article: Article;
 
   constructor() {
-    this.title = 'Angular 2';
-    this.link = 'http://angular.io';
-    this.votes = 10;
-  } 
+    this.article = new Article(
+      'Angular',
+      'http://angular.io',
+      10
+    );
+  }
 
   voteUp(): boolean {
-    this.votes += 1;
+    this.article.voteUp();
 
     return false;
   }
   
   voteDown(): boolean {
-    this.votes -= 1;
+    this.article.voteDown();
 
     return false;
   }
